86b5d946af14fc2f1bef504eff2deccfc72e644a
Ali Zair <az2109964@qu.edu.qa>
2024-09-21 08:17:26 +0300

    fix next config to not prepend repo name in development build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-	basePath: "/website-main",
-	assetPrefix: "/website-main",
+	basePath: isProd ? "/website-main" : "",
+	assetPrefix: isProd ? "/website-main" : "",
 	images: {
 		unoptimized: true,
 	},
