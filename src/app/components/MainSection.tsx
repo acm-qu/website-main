d8ce103c6eb5769c27c7026e9b43be270ad04cd4
Ali Zair <az2109964@qu.edu.qa>
2024-10-15 20:31:25 +0300

    add responsiveness to the main section

--- src/app/components/MainSection.tsx
+++ src/app/components/MainSection.tsx
@@ -4,27 +4,33 @@ import { motion } from "framer-motion";
 
 export default function MainSection() {
 	return (
-		<section id="about" className="top-down-gradient min-h-[700px]">
-			<div className="flex">
+		<section
+			id="about"
+			className="top-down-gradient min-h-[500px] lg:min-h-[700px]"
+		>
+			<div className="flex flex-col md:flex-row items-center md:items-start">
 				<motion.h1
 					initial={{ x: -100, opacity: 0 }}
 					animate={{ x: 0, opacity: 1 }}
 					transition={{ duration: 1.2 }}
-					className="text-5xl font-bold mt-52 ml-24 w-1/2"
+					className="text-3xl md:text-4xl lg:text-5xl font-bold mt-14 md:mt-44 lg:mt-52 md:ml-24 w-full md:w-1/2 text-center md:text-left"
 				>
-					ASSOCIATION FOR COMPUTING MACHINERY, QATAR UNIVERSITY CHAPTER
+					<p className="mr-10 ml-10">
+						ASSOCIATION FOR COMPUTING MACHINERY, QATAR UNIVERSITY CHAPTER
+					</p>
 				</motion.h1>
 				<motion.div
 					initial={{ opacity: 0, scale: 0.8 }}
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{ duration: 1.2, delay: 0.5 }}
+					className="mt-10 md:mt-0"
 				>
 					<Image
 						src="images/img.png"
 						alt="Logo"
 						width={600}
 						height={600}
-						className="mr-10"
+						className="w-48 h-48 md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px] mx-auto md:mr-10 md:mt-10"
 						priority
 					/>
 				</motion.div>
@@ -33,7 +39,7 @@ export default function MainSection() {
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
 				transition={{ duration: 1.4, delay: 0.8 }}
-				className="ml-24 -mt-40 text-xl"
+				className="ml-5 mr-5 md:ml-32 text-lg md:text-xl text-center md:text-left"
 			>
 				Connecting students with the latest in technology and innovation.
 			</motion.p>
